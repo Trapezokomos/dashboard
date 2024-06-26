e1b8e8bf2e4f25739d40deef22549381479a4719
spatiszisis <zisispatis@gmail.com>
2024-06-26 17:50:40 +0300

    - Remove comments

--- src/test/java/net/trapezokomos/dashboard/DashboardApplicationTests.java
+++ src/test/java/net/trapezokomos/dashboard/DashboardApplicationTests.java
@@ -3,11 +3,11 @@ package net.trapezokomos.dashboard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class DashboardApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }
