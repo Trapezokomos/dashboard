7c2c479a4b0e5a1dd00c375a17a153bad00e1838
spatiszisis <zisispatis@gmail.com>
2024-06-26 13:50:02 +0300

    #1 Basic Project Structure - Added docker-compose.yml that include the db postgresql - Added swagger - Added flyway for database version, also added a init file see V1__initialschema.sql

--- src/main/java/net/trapezokomos/dashboard/DashboardApplication.java
+++ src/main/java/net/trapezokomos/dashboard/DashboardApplication.java
@@ -1,5 +1,6 @@
 package net.trapezokomos.dashboard;
 
+import net.trapezokomos.dashboard.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DashboardApplication {
 
     public static void main(String[] args) {
+        EnvConfig.loadEnv();
         SpringApplication.run(DashboardApplication.class, args);
     }
 
